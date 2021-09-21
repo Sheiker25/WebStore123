e24189a6e4e5baef0ec2d86d6c58c00d5fab6d00
Sheiker25 <DeniShee@yandex.ru>
2021-09-21 20:52:11 +0300

    Чистка кода от хлама.

--- WebStore123/Program.cs
+++ WebStore123/Program.cs
@@ -1,11 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace WebStore123
 {
@@ -13,14 +8,17 @@ namespace WebStore123
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host_builder = CreateHostBuilder(args);
+            var host = host_builder.Build();
+            host.Run();
+
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(host => host
+                    .UseStartup<Startup>()
+                );
     }
 }
