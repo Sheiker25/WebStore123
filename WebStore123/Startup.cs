656bd778a91b219352f7856a0902ce7f1eb2b3b7
Sheiker25 <DeniShee@yandex.ru>
2021-09-22 14:23:31 +0300

    Добавление переменной

--- WebStore123/Startup.cs
+++ WebStore123/Startup.cs
@@ -1,49 +1,47 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace WebStore123
 {
     public class Startup
     {
         public IConfiguration Configuration { get; set; }
+
         public Startup(IConfiguration Configuration)
         {
             this.Configuration = Configuration;
         }
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            
         }
 
-        
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-            }
 
-            app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            var greetings = "Hello from my first ASP.NET";
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapGet("/", async context =>
+                    {
+                        await context.Response.WriteAsync(greetings);
+                    });
             });
         }
     }
+
 }
